83da901f3e041dbc2c93c5274d226e5172c01aa6
AD1616 <sahilsamar031@gmail.com>
2022-04-08 13:19:15 -0700

    added ghusers to model

--- cruddy/model.py
+++ cruddy/model.py
@@ -1,12 +1,8 @@
-""" database dependencies to support Users db examples """
 from __init__ import db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
-
 # Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along
-
 # Define the Users table within the model
 # -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
 # -- a.) db.Model is like an inner layer of the onion in ORM
@@ -19,14 +15,14 @@ class Users(UserMixin, db.Model):
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), unique=False, nullable=False)
     phone = db.Column(db.String(255), unique=False, nullable=False)
-
+    ghuser = db.Column(db.String(255), unique=False, nullable=False)
     # constructor of a User object, initializes of instance variables within object
-    def __init__(self, name, email, password, phone):
+    def __init__(self, name, email, password, phone, ghuser):
         self.name = name
         self.email = email
         self.set_password(password)
         self.phone = phone
-
+        self.ghuser = ghuser
     # CRUD create/add a new record to the table
     # returns self or None on error
     def create(self):
@@ -38,7 +34,6 @@ class Users(UserMixin, db.Model):
         except IntegrityError:
             db.session.remove()
             return None
-
     # CRUD read converts self to dictionary
     # returns dictionary
     def read(self):
@@ -48,12 +43,12 @@ class Users(UserMixin, db.Model):
             "email": self.email,
             "password": self.password,
             "phone": self.phone,
+            "ghuser": self.ghuser,
             "query": "by_alc"  # This is for fun, a little watermark
         }
-
     # CRUD update: updates users name, password, phone
     # returns self
-    def update(self, name, password="", phone=""):
+    def update(self, name, password="", phone="", ghuser=""):
         """only updates values with length"""
         if len(name) > 0:
             self.name = name
@@ -61,49 +56,43 @@ class Users(UserMixin, db.Model):
             self.set_password(password)
         if len(phone) > 0:
             self.phone = phone
+        if len(ghuser) > 0:
+            self.ghuser = ghuser
         db.session.commit()
         return self
-
     # CRUD delete: remove self
     # None
     def delete(self):
         db.session.delete(self)
         db.session.commit()
         return None
-
     # set password method is used to create encrypted password
     def set_password(self, password):
         """Create hashed password."""
         self.password = generate_password_hash(password, method='sha256')
-
     # check password to check versus encrypted password
     def is_password_match(self, password):
         """Check hashed password."""
         result = check_password_hash(self.password, password)
         return result
-
     # required for login_user, overrides id (login_user default) to implemented userID
     def get_id(self):
         return self.userID
-
-
 """Database Creation and Testing section"""
-
-
 def model_tester():
     print("--------------------------")
     print("Seed Data for Table: users")
     print("--------------------------")
     db.create_all()
     """Tester data for table"""
-    u1 = Users(name='Thomas Edison', email='tedison@example.com', password='123toby', phone="1111111111")
-    u2 = Users(name='Nicholas Tesla', email='ntesla@example.com', password='123niko', phone="1111112222")
-    u3 = Users(name='Alexander Graham Bell', email='agbell@example.com', password='123lex', phone="1111113333")
-    u4 = Users(name='Eli Whitney', email='eliw@example.com', password='123whit', phone="1111114444")
-    u5 = Users(name='John Mortensen', email='jmort1021@gmail.com', password='123qwerty', phone="8587754956")
-    u6 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="8587754956")
+    u1 = Users(name='Thomas Edison', email='tedison@example.com', password='123toby', phone="1111111111", ghuser="TEddy")
+    u2 = Users(name='Nicholas Tesla', email='ntesla@example.com', password='123niko', phone="1111112222", ghuser="IDontMakeCars")
+    u3 = Users(name='Alexander Graham Bell', email='agbell@example.com', password='123lex', phone="1111113333", ghuser="WhoNow")
+    u4 = Users(name='Eli Whitney', email='eliw@example.com', password='123whit', phone="1111114444", ghuser="MrMort")
+    u5 = Users(name='John Mortensen', email='jmort1021@gmail.com', password='123qwerty', phone="8587754956", ghuser="MrMortSmurf")
+    u6 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="8587754956", ghuser="OtherMort")
     # U7 intended to fail as duplicate key
-    u7 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="8586791294")
+    u7 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="8586791294", ghuser="IsThisDifferent")
     table = [u1, u2, u3, u4, u5, u6, u7]
     for row in table:
         try:
@@ -112,8 +101,6 @@ def model_tester():
         except IntegrityError:
             db.session.remove()
             print(f"Records exist, duplicate email, or error: {row.email}")
-
-
 def model_printer():
     print("------------")
     print("Table: users with SQL query")
@@ -122,8 +109,6 @@ def model_printer():
     print(result.keys())
     for row in result:
         print(row)
-
-
 if __name__ == "__main__":
     model_tester()  # builds model of Users
     model_printer()
